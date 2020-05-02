5915b992e50ffd1bb3710bd91f1c15002cbe70f6
karollynecosta <karolcosta2@gmail.com>
2020-05-02 00:13:38 -0300

    ajustes port

--- front-end/src/pages/dev/index.js
+++ front-end/src/pages/dev/index.js
@@ -1,11 +1,29 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 import Container from '../../components/Container/';
+import Dev from '../../assets/dev.jpg';
+import { Profile, Button } from './styles';
 
 export default function dev() {
   return (
     <Container>
       <h1>Desenvolvedora</h1>
+      <Profile>
+        <img src={Dev} alt="Karol" />
+        <p>
+          Sou formada em Gestão de TI e Pós-Graduada em Gestão e Segurança de
+          Redes de Computadores. Sempre tive afinidade com a tecnologia, nesta
+          etapa da vida estou estudando a fundo desenvolvimento web através do
+          JavaScript.
+        </p>
+        <Link>
+          <Button>
+            <a href="https://www.linkedin.com/in/karollyne-costa-2a185aa7/">
+              Linkedin
+            </a>
+          </Button>
+        </Link>
+      </Profile>
     </Container>
   );
 }
--- front-end/src/pages/dev/styles.js
+++ front-end/src/pages/dev/styles.js
@@ -1,3 +1,19 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Profile = styled.div`
+  img {
+    border-radius: 50%;
+    height: 200px;
+    width: 200px;
+    float: right;
+  }
+
+  p {
+    font-size: 16px;
+    font-family: sans-serif;
+    text-align: justify;
+    padding: 10px;
+  }
+`;
+
+export const Button = styled.button``;
